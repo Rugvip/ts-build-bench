b43260c9734362150ddc407cca8d3b406e541e99
Patrik Oldsberg <poldsberg@gmail.com>
2020-05-10 12:30:17 +0200

    benchmark: added dimension presets

--- benchmark.js
+++ benchmark.js
@@ -1,5 +1,5 @@
-const { createProject } = require('./factory');
-const { ProjectRunner } = require('./runner');
+const { createProjectMatrix } = require('./factory');
+const { MatrixRunner } = require('./runner');
 
 const presets = {
   components: {
@@ -27,21 +27,74 @@ const presets = {
       components: presets.components.balanced(n),
     }),
   },
-};
-
-async function main() {
-  const project = await createProject({
-    path: 'workdir/bench-1',
+  baseConfig: () => ({
+    path: 'workdir/bench',
     main: 'dist/index.js',
     types: 'dist/index.d.ts',
     componentExports: 'default', // or named
     packages: Array(5).fill(presets.packages.balanced(5)),
+  }),
+  dimensions: {
+    packageSizes: () => ({
+      small: {
+        packages: Array(5).fill(presets.packages.balanced(3)),
+      },
+      medium: {
+        packages: Array(10).fill(presets.packages.balanced(10)),
+      },
+      large: {
+        packages: Array(20).fill(presets.packages.balanced(20)),
+      },
+    }),
+    muiImportMethod: () => ({
+      muiIndex: {
+        packages: Array(5).fill({
+          libs: presets.libs.balanced(1),
+          components: presets.components.normalImports(4),
+        }),
+      },
+      muiSpecific: {
+        packages: Array(5).fill({
+          libs: presets.libs.balanced(1),
+          components: presets.components.specificImports(4),
+        }),
+      },
+    }),
+    typesEntrypoint: () => ({
+      typesDist: {
+        types: 'dist/index.d.ts',
+      },
+      typesSrc: {
+        types: 'src/index.ts',
+      },
+    }),
+    componentExports: () => ({
+      exportDefault: {
+        componentExports: 'default',
+      },
+      exportNamed: {
+        componentExports: 'named',
+      },
+    }),
+  },
+};
+
+async function main() {
+  const projects = await createProjectMatrix({
+    baseConfig: presets.baseConfig(),
+    dimensions: [
+      presets.dimensions.muiImportMethod(),
+      presets.dimensions.typesEntrypoint(),
+      presets.dimensions.componentExports(),
+    ],
   });
 
-  const r = new ProjectRunner(project);
+  const r = new MatrixRunner(projects);
   await r.prepare();
   const buildTime = await r.timeCmd('.', ['yarn', 'build']);
-  console.log('DEBUG: buildTime =', buildTime);
+  console.log('buildTime =', buildTime);
+  const typecheckTime = await r.timeCmd('.', ['yarn', 'main:typecheck']);
+  console.log('typecheckTime =', typecheckTime);
 }
 
 main().catch((error) => {
